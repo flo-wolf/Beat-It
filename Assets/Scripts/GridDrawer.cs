cc17ecb87f64ed5c50ee916820a0e30e0e4c59e6
Florian Wolf <fw.flolf@gmail.com>
2018-05-23 00:14:37 +0200

    Grid Finished, resulting in a working grid on which the player can move from intersection to intersection by using the keyboardkeys to move around.

--- Assets/Scripts/GridDrawer.cs
+++ Assets/Scripts/GridDrawer.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridDrawer : MonoBehaviour {
+    public Material lineMaterial;
 
-    public LineRenderer lineRenderer;
 
-    private Material lineMaterial;
 
     public Color mainColor = new Color(0f, 1f, 0f, 1f);
-    public Color subColor = new Color(0f, 0.5f, 0f, 1f);
 
     // Use this for initialization
     void Start () {
-	}
+        
+    }
 
     void CreateLineMaterial()
     {
-
         if (!lineMaterial)
         {
             // Unity has a built-in shader that is useful for drawing
@@ -31,8 +29,11 @@ public class GridDrawer : MonoBehaviour {
             // Turn backface culling off
             lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
             // Turn off depth writes
-            lineMaterial.SetInt("_ZWrite", 0);
+            lineMaterial.SetInt("_ZWrite", 3);
+            lineMaterial.SetInt("_ZTest", (int)UnityEngine.Rendering.CompareFunction.Less);
+            //lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
         }
+        
     }
 
     void OnPostRender()
@@ -43,26 +44,48 @@ public class GridDrawer : MonoBehaviour {
 
         GL.Begin(GL.LINES);
 
-        GL.Color(subColor);
+        GL.Color(mainColor);
 
         int xCount = InputInterpreter.keybaordMap[0].Length;
-        int yCount = InputInterpreter.keybaordMap.Length;
+        int yCount = InputInterpreter.keybaordMap.Length - 1;
 
         // lineRenderer.positionCount = InputInterpreter.keybaordMap.Length * InputInterpreter.keybaordMap[0].Length;
 
         //X axis lines (top to bottom)
-        for (float i = 0; i <= xCount; i ++)
+        for (float i = 0; i < xCount; i ++)
         {
-            Vector3 start = new Vector3(1 + (i * 2) / 16, 1f - (3f / 18f), 1);
-            Vector3 end = new Vector3(1 + ((i * 2) + yCount * 0.5f) / 16, (1f - ((3f + (yCount * 4f)) / 18f)), 1);
+            Vector3 start = new Vector3((1+(i * 2)) / 16, 1f - (3f / 18f), 1);
+            Vector3 end = new Vector3((1+((i * 2) + yCount * 0.5f)) / 16, (1f - ((3f + (yCount * 4f)) / 18f)), 1);
+
+            start = Camera.main.ViewportToWorldPoint(start);
+            end = Camera.main.ViewportToWorldPoint(end);
+
+            start.z = 1;
+            end.z = 1;
 
             //GL.Vertex3((m * (1 + (i * 2))) / 16, m * (1f - (3f / 18f)), 1);
             //GL.Vertex3(m * ((1 + (i * 2)) + yCount * 0.5f) / 16, m * (1f - ((3f + (yCount * 4f)) / 18f)), 1);
 
             GL.Vertex3(start.x, start.y, start.z);
             GL.Vertex3(end.x, end.y, end.z);
+        }
 
-            Debug.Log(start);
+        //Y axis lines (left to right)
+        for (float i = 0; i <= yCount; i++)
+        {
+            Vector3 start = new Vector3((1f + i * 0.5f) / 16, (1f - ((3f + (i * 4f)) / 18f)), 1);
+            Vector3 end = new Vector3(((1f + i * 0.5f) + (xCount*2f - 2)) / 16,
+                (1f - ((3f + (i * 4f)) / 18f)),
+                1);
+
+            start = Camera.main.ViewportToWorldPoint(start);
+            end = Camera.main.ViewportToWorldPoint(end);
+
+            start.z = 1;
+            end.z = 1;
+
+            GL.Vertex3(start.x, start.y, start.z);
+            GL.Vertex3(end.x, end.y, end.z);
         }
 
         /*
