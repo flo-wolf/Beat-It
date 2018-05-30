4a3be78d6c60750e28f061971d9e8a2b83c327bc
Florian Wolf <fw.flolf@gmail.com>
2018-05-30 02:36:37 +0200

    Chages to the spawning system, preparing for the consideration of two loop dots instead of just one while finding a suitable spawning position

--- Assets/Scripts/DotSpawner.cs
+++ Assets/Scripts/DotSpawner.cs
@@ -3,44 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DotSpawner : MonoBehaviour {
+
 	//public stuff
-	public Dot Dot;
-	public Dot DotStart;
-    public int LevelInstanceSize = 5;
+	public Dot dot;
+	public Dot dotStart;
+    public int MaxDots = 5;
 
     public int rythmSpawnSkip = 4;
 
     private int rythmSpawnSkipCount = 4;
-    private int LevelCounter = 0;
-    //counter for naming the Dots
-    private int counter = 2;
+    private int DotCount = 0;
 
-	//private stuff
-	List<Dot> DotList = null;
-	LineRenderer connection;
-	List<LineRenderer> connections = null;
-	//spawn delay
-	//ADD IN THE GLOBAL BEAT THINGY
-	public float delay;
-	float timer;
-	//random values
-	float randomRadius;
-	float randomAngle;
-	//Spawnvector
-	Vector3 spawnVector;
+    //private stuff
+    private List<Dot> DotList = new List<Dot>();
+	private LineRenderer connection;
+    private List<LineRenderer> connections = new List<LineRenderer>();
+
+    //random values used for calculating a suitable spawn position
+    private float randomRadius;
+    private float randomAngle;
+
 	// Use this for initialization
-	void Start () {
-		DotStart._OC = DotStart.GetComponent<LineRenderer> ();
-		DotList = new List<Dot> ();
-		DotList.Add (this.DotStart);
-		connection = this.DotStart.GetComponent<LineRenderer> ();
-		connections = new List<LineRenderer> ();
+	void Start ()
+    {
+		DotList.Add (dotStart);
+		connection = dotStart.GetComponent<LineRenderer>();
 
         RythmManager.onRythm.AddListener(Spawn);
     }
 		
 	//Method to connect 2 points with a linerenderer
-	void connect (Dot currentDot , Dot nextDot)
+	void Connect (Dot currentDot , Dot nextDot)
 	{
 		Vector3[] positions = new Vector3[] {currentDot.transform.position , nextDot.transform.position};
 		//OC = Outgoing Connection
@@ -61,57 +54,56 @@ public class DotSpawner : MonoBehaviour {
         else
             rythmSpawnSkipCount = 0;
 
-            //choose random Dot from list
-            int randomDot = Random.Range(0,this.DotList.Count - 1);
+        //choose random Dot from list
+        int randomDot = Random.Range(0, DotList.Count - 1);
+
 		//get the random Dot from the list
 		Dot currentDot = DotList[randomDot];
 
-		// GLOBAL RYTHM
-		this.castRays( currentDot );
+		// Find a suitable spawn location
+		Vector3 spawnVector = CastRays(currentDot);
+
 		bool isGoodLocation = true;
-		foreach (Dot node in this.DotList) {
-			if (this.spawnVector == node.transform.position)
+		foreach (Dot node in DotList) {
+			if (spawnVector == node.transform.position)
 				isGoodLocation = false;
 		}
 
-        if (isGoodLocation && this.LevelCounter < this.LevelInstanceSize - 1)
+        if (isGoodLocation && DotCount < MaxDots - 1)
 		{
 
             Debug.Log("1111 ");
 			//suck in outgoing connection
-			this.suckIn(randomDot , currentDot);
+			SuckIn(randomDot , currentDot);
 
 			//cast rays
             //this.castRays( currentDot );
 
 			//create new node
+			Dot nextDot = Instantiate (dot, spawnVector, Quaternion.Euler( new Vector3 (-180,0,0)));
+            nextDot.transform.parent = transform;
 
-			Dot nextDot = Instantiate (this.Dot, this.spawnVector, Quaternion.Euler( new Vector3 (-180,0,0)));
-            nextDot.transform.parent = this.transform;
-            nextDot.name = "LevelDot " + counter.ToString();
-            this.LevelCounter++;
+            DotCount++;
+
+            nextDot.name = "LevelDot " + DotCount.ToString();
 			//add to list
 			DotList.Insert (randomDot + 1, nextDot);
 			//name it ( i cant change the name of the child of the new Dots :C )
-			nextDot.gameObject.transform.GetChild(0).name = System.Convert.ToString ("Blockradius" + this.counter);
-
-			this.counter++;
+			nextDot.gameObject.transform.GetChild(0).name = System.Convert.ToString ("Blockradius" + DotCount);
 
 			//connect the old point with the new one
-			this.connect (currentDot, nextDot);
-            
-			timer = 0;
+			Connect (currentDot, nextDot);
+
 			//reconnect the new Dot with the one wich has no incoming connection
-			this.reconnect (randomDot + 1, nextDot);
+			Reconnect (randomDot + 1, nextDot);
 			isGoodLocation = true;
 		}
-
-
 	}
 
-	void castRays(Dot node)
+    // Find a suitable spawn vector and set spawnVector to that found vector
+	Vector2 CastRays(Dot node)
     {
-		//node list - ignore the current node and its blockradius!!!
+		//the ray has hit the level boundaries (block radius)
 		bool hitBlockRadius = false;
 		//default local direction
 		Vector3 direction = new Vector3(0,1,0);
@@ -127,8 +119,9 @@ public class DotSpawner : MonoBehaviour {
 		//first ray needs to be shot seperatly
 		//store all objects in the array
 		RaycastHit2D[] hitArray = Physics2D.RaycastAll (node.transform.position, direction, node.radius);
+
 		//check if the hitarray DOES NOT CONTAIN A BLOCKRADIUS OR A WALL
-		for (int i = 0; i < hitArray.Length - 1; i++) {
+		for (int i = 0; i < hitArray.Length-1; i++) {
 			if ((hitArray[i].collider.tag == "blockradius") || (hitArray[i].collider.tag == "wall"))
         	{
 				//If there is a BLOCKRADIUS OR A WALL then flag the array as not spawnable
@@ -137,9 +130,12 @@ public class DotSpawner : MonoBehaviour {
 				goto NEXT;
         	}
 		}
+
 		//adding the possible spawnpoint if it's a good one
 		if (!hitBlockRadius)
 			hit.Add (spawnpoint + direction.normalized * node.radius);
+
+        // the first try failed. Now we iterate around the node and try to find a suitable spawn location.
 		NEXT:
 		//increase angle and set hitBlockRadius to false
 		turnAngle += 10f;
@@ -153,8 +149,10 @@ public class DotSpawner : MonoBehaviour {
 			//get all objects in the ray's way 
 			hitArray = Physics2D.RaycastAll (node.transform.position, direction, node.radius);
 			spawnpoint = node.transform.position + direction.normalized * node.radius;
+
 			//check all the objects if there is a blockradius or a wall
-			for (int j = 0; j < hitArray.Length - 1; i++) {
+			for (int j = 0; j < hitArray.Length - 1; i++)
+            {
 				if ((hitArray[j].collider.tag == "blockradius") || (hitArray[j].collider.tag == "wall"))
 				{
 					//If so flag it as not usable and goto JUMP
@@ -172,20 +170,24 @@ public class DotSpawner : MonoBehaviour {
 			
 		//choose random vector from the list
 		int randomVector = Random.Range(0, hit.Count - 1);
-		//override internal spawnvector
-		//there are cases where the list is empty
-		if (hit.Count != 0)
-			this.spawnVector = hit [randomVector];
+        //override internal spawnvector
+        //there are cases where the list is empty
+        if (hit.Count != 0)
+            return hit[randomVector];
+        else
+            return node.transform.position;
     }
 
-	void suckIn(int Index , Dot currentDot)
+	void SuckIn(int Index , Dot currentDot)
 	{
 		//if the Index is bigger than 0/ not the start of the list
 		//basically set end and start point of linerenderer to the point so it's not visible anymore
-		if (Index > 0) {
-			Vector3[] position = new Vector3[] {
-				this.DotList [Index - 1].transform.position,
-				this.DotList [Index - 1].transform.position
+		if (Index > 0)
+        {
+			Vector3[] position = new Vector3[] 
+            {
+				DotList [Index - 1].transform.position,
+				DotList [Index - 1].transform.position
 			};
 			DotList [Index + 1].hasIC = false;
 			currentDot._OC.SetPositions (position);
@@ -193,41 +195,44 @@ public class DotSpawner : MonoBehaviour {
 		//If it's the start of the list
 		else if ( Index == 0 )
 		{
-			Vector3[] position = new Vector3[] {
-				this.DotList [0].transform.position ,
-				this.DotList [0].transform.position
+			Vector3[] position = new Vector3[] 
+            {
+				DotList [0].transform.position ,
+				DotList [0].transform.position
 			};
-			this.DotList [0].hasIC = false;
+			DotList [0].hasIC = false;
 		}
 	}
 
 	//throw new point
-	void throwPoint()
+	void ThrowPoint()
 	{
 
 	}
 
 	//checks if it has IC ( pretty useless :P )
-	bool getIC(Dot currentDot)
+	bool GetIC(Dot currentDot)
 	{
 		return currentDot.hasIC;
 	}
+
 	//reconnects the missing connections
-	void reconnect(int Index , Dot nextDot )
+	void Reconnect(int Index , Dot nextDot )
 	{
 		//set the linerenderers endposition to the next Dot's position
-		if (Index < this.DotList.Count - 1) {
-			Vector3[] positions = new Vector3[] { nextDot.transform.position, this.DotList [Index + 1].transform.position };
+		if (Index < DotList.Count - 1)
+        {
+			Vector3[] positions = new Vector3[] { nextDot.transform.position, DotList [Index + 1].transform.position };
 			nextDot._OC = nextDot.GetComponent<LineRenderer> ();
 			nextDot._OC.SetPositions (positions);
 			Debug.Log ("Reconnected to " + DotList [Index + 1].name);
-		} else if (Index == this.DotList.Count - 1 || Index >= this.DotList.Count - 1) {
-			Vector3[] positions = new Vector3[] { nextDot.transform.position , this.DotList [ 0 ].transform.position };
+		} else if (Index == DotList.Count - 1 || Index >= DotList.Count - 1)
+        {
+			Vector3[] positions = new Vector3[] { nextDot.transform.position , DotList [ 0 ].transform.position };
 			nextDot._OC = nextDot.GetComponent<LineRenderer> ();
 			nextDot._OC.SetPositions (positions);
 			Debug.Log ("Reconnected to " + DotList [0].name);
 		}
-
 	}
 
 	// Update is called once per frame
