871615be09f9d91f4e3903adaee92906677756af
Florian Wolf <fw.flolf@gmail.com>
2018-06-11 13:58:27 +0200

    Fixed Player Movement. Try it out!

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -45,7 +45,7 @@ public class Grid : MonoBehaviour
         for(int i = 0; i < tries; i++)
         {
             g = gridDots[Random.Range(0, instance.rows - 1), Random.Range(0, instance.columns - 1)];
-            if (g != null && g.active && g.levelObj == null)
+            if (g != null && g.active && g.levelObj == null && g.gameObject.activeSelf)
                 return g;
         }
         return g;
@@ -54,31 +54,6 @@ public class Grid : MonoBehaviour
     // get the nearest dot based on a normalized input (thumbstick to the right => closest active dot on the right)
 	public static GridDot GetNearestActiveDot(GridDot dot, Vector2 direction)
     {
-        Vector2 aimPos;
-        // mouse input
-        if (InputDeviceDetector.inputType == InputDeviceDetector.InputType.MouseKeyboard)
-        {
-            // get the world mouse position 
-            Vector2 mousePos = Input.mousePosition;
-            aimPos = Camera.main.ScreenToWorldPoint(mousePos);
-
-            aimPos = (aimPos - (Vector2)dot.transform.position).normalized;
-        }
-
-        // controller input
-        else
-        {
-            Vector2 controllerInput = new Vector2(Input.GetAxis("Joystick X"), Input.GetAxis("Joystick Y"));
-
-            aimPos = controllerInput;
-
-            // check which point is closest to the lookdirection
-            aimPos = (aimPos - (Vector2)dot.transform.position).normalized;
-        }
-
-        direction = aimPos;
-
-        //direction = direction;
 
         // gather all six sourrounding dots
         List<GridDot> hexDots = new List<GridDot>();
@@ -133,18 +108,22 @@ public class Grid : MonoBehaviour
                 hexDots.Add(gridDots[dot.row + 1, dot.column]);
         }
 
+        direction = direction.normalized;
+        direction = (Vector2)dot.transform.position + direction;
 
-        Vector2 lookPos = direction - (Vector2)dot.transform.position;
+        Debug.Log("Nearest Active - centerDot: " + dot.transform.position + " direction: " + direction);
+
         GridDot closestDot = null;
         float closestLength = 100000f;
 
         foreach (GridDot d in hexDots)
         {
             // rule out all the dots that are not set to be active
-            if (d.active && d.levelObj == null)
+            if (d.active && d.levelObj == null && d.gameObject.activeSelf)
             {
+                Debug.Log("Hexdot Possible: " + d.transform.position);
                 // get distance between the sourrounding hexagon dot and our dot position plus the lookdirection
-                float l = (lookPos - (Vector2)d.transform.position).magnitude;
+                float l = (direction - (Vector2)d.transform.position).magnitude;
                 if(l < closestLength)
                 {
                     closestLength = l;
