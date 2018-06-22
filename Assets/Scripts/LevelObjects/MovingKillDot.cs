d63cabc3d307933135908add4275405d8eb4be3a
SVENLAPTOP\Sven <schmid.sven@hotmail.com>
2018-06-22 14:23:33 +0200

    Added Moving Kill Prefab again. Moved Scripts from Assets into script folder.

---
+++ Assets/Scripts/LevelObjects/MovingKillDot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingKillDot : LevelObject {
+
+    ParticleSystem killFeedback;
+
+    bool kill = false;
+
+    private void Start()
+    {
+        killFeedback = GetComponent<ParticleSystem>();
+        RythmManager.onBPM.AddListener(OnRythmCount);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerSegment.touchedKillDot = true;
+            Player.allowMove = false;
+            Game.SetState(Game.State.Death);
+
+            killFeedback.Play();
+        }
+    }
+
+    void OnRythmCount(BPMinfo bpm)
+    {
+        if (bpm.Equals(RythmManager.playerBPM))
+        {
+            if (kill || (Player.dot0 != null && Player.dot0.transform.position == transform.position) || (Player.dot1 != null && Player.dot1.transform.position == transform.position))
+            {
+                AudioManager.instance.Play("Death");
+                kill = false;
+            }
+        }
+    }
+}
