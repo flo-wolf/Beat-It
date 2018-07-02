d18ac642f13b7348b03435e932717000cd48cd03
Florian Wolf <fw.flolf@gmail.com>
2018-07-02 16:12:43 +0200

    Added Television post processing. Changed sprites to fit the new style. Added an animation script, allowing moving static FX. Reactivated music.

---
+++ Assets/AnimateScreenFX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VortexStudios.PostProcessing;
+
+public class AnimateScreenFX : MonoBehaviour {
+
+    OLDTVFilter3 filter = null;
+    public float offsetChangePerUpdate = 0.0001f;
+
+    OLDTVPreset memoryPreset = null;
+
+    // Use this for initialization
+    void Start () {
+        filter = GetComponent<OLDTVFilter3>();
+        filter.preset.staticFilter.staticOffset = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        filter.preset.staticFilter.staticOffset += offsetChangePerUpdate;
+    }
+}
